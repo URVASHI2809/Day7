be7348675872fdff5866beb7af38aa99ce079835
Urvashi Prajapati <urvaship2809@gmail.com>
2022-04-06 00:43:24 +0530

    Day 7

---
+++ user-service/src/main/java/com/rakuten/UserController.java
@@ -0,0 +1,26 @@
+package com.rakuten;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	List<User> users = new ArrayList<>();
+	
+    @PostMapping
+     void saveUser(@RequestBody User user) {
+    	System.out.println(user.getName());
+    	System.out.println(user.getAge());
+    	users.add(user);
+    }
+    @GetMapping
+    List<User> getUsers() {
+    	return users;
+    }
+}
